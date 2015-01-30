0c5be549c433eb2b64940f1404453366efa83d47
Israel Zuñiga <israelzdlm@gmail.com>
2015-01-29 19:24:53 -0600

    Added sketch.py

---
+++ head1stpython/Chapter3/sketch.py
@@ -0,0 +1,14 @@
+#Import dependencies
+#Load OS functions from the standard library
+import os
+
+os.chdir('/home/israel/Development/Python_Exercises/python-octo-wookie/head1stpython/Chapter3')
+#Change path for the current directory
+
+data = read('sketch.txt)
+#Start iteration over the text file
+for each_line in data:
+	(role, line_spoken) = each_line.split(':', 1)
+	print(role, end = '')
+	print(' said: ', end = '')
+	print(line_spoken, end = '')
